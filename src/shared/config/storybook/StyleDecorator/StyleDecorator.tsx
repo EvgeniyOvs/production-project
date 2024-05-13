a818ff1fba02ba62b6c5724b8bfd6c9238912baf
EvgeniyOvs <126809915+EvgeniyOvs@users.noreply.github.com>
2024-05-13 22:00:24 +0300

    add main pipeline github actions

---
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.tsx
@@ -0,0 +1,9 @@
+import 'app/styles/index.scss';
+import { Story } from '@storybook/react';
+
+export const StyleDecorator = (StoryComponent: Story) => (
+    <div>
+        <StoryComponent />
+    </div>
+
+);
